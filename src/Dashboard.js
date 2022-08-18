568cea3642b0c8019fc176713b2e3160b8c7fdfe
Tridwan <iamtridwan@gmail.com>
2022-08-18 14:33:45 +0100

    initial setup file

---
+++ src/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div style={dashboardStyle}>
+
+    </div>
+  )
+}
+
+const dashboardStyle = {
+    with: "100%",
+    backgroundColor: "#FAFAFE",
+    fontFamily: "Poppins",
+    padding: "5px",
+}
+export default Dashboard
